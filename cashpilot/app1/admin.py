92fced3a4cf2ff36706b3d0e1bf140af2ff9a9f3
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2025-09-17 10:12:35 -0300

    registrando as classes no admin

--- cashpilot/app1/admin.py
+++ cashpilot/app1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Entradas, Saidas, Saldo
 # Register your models here.
+admin.site.register(Entradas)
+admin.site.register(Saidas)
+admin.site.register(Saldo)
