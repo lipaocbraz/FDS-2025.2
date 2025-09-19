c342a85ce0158fc53d84de803cd083d2d02876e8
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2025-09-19 11:09:47 -0300

    tentando resolver o problema anterior

--- cashpilot/users/urls.py
+++ cashpilot/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as vs
 
-urlparttners = [
+urlpatterns = [
     path('login', vs.LoginView.as_view(template_name = 'users/login.html'), name='login'), 
     path('logout', vs.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
 ]
