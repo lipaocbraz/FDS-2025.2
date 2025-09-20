ecb5c5e0d221eb59836406f2c1f98acbb2364198
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2025-09-20 12:22:32 -0300

    criando pagina de cadastro de usuarios

--- cashpilot/users/urls.py
+++ cashpilot/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as vs
+from . import views
 
 urlpatterns = [
     path('login', vs.LoginView.as_view(template_name = 'users/login.html'), name='login'), 
     path('logout', vs.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
+    path('cadastro',views.cadastro, name='cadastro'),
 ]
--- cashpilot/users/views.py
+++ cashpilot/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import login,authenticate
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+def cadastro(request):
+    if request.method != 'POST':
+        form=UserCreationForm()
+    else:
+        form=UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user=form.save()
+            authenticated_user=authenticate(username=new_user.username,password=request.POST['password1'])
+            login(request,authenticated_user)
+            return HttpResponseRedirect(reverse('index'))
+    context={'form':form}
+    return render(request,'users/cadastro.html',context)
