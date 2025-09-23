7ab75fcfd60d8a888ca3d0b703f24469fe6a2040
Bernardo Cavalcanti Carneiro Leão <bernardocavalcanticarneiroleao@MacBook-Air-de-Bernardo.local>
2025-09-23 09:31:37 -0300

    concertando mais um caminho

--- cashpilot/users/urls.py
+++ cashpilot/users/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as vs
 from . import views
 
 urlpatterns = [
-    path('login', vs.LoginView.as_view(template_name = 'users/login.html'), name='login'), 
+    path('login', vs.LoginView.as_view(template_name = 'users/html/login.html'), name='login'), 
     path('logout', views.logout_view, name='logout'),
     path('cadastro',views.cadastro, name='cadastro'),
 ]
