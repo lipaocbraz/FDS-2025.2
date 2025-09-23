116a689f26ea0f26690958c5575fdab23cb93daa
Luiz-Eduardo241106 <edudearaujomariz@gmail.com>
2025-09-23 11:59:14 -0300

    Criando o forms para login.

--- cashpilot/users/formsLogin.py
+++ cashpilot/users/formsLogin.py
@@ -1,10 +1,19 @@
 from django import forms
+from django.contrib.auth import authenticate
+from django.core.exceptions import ValidationError
 
-class loginForm(forms.Form):
-    login = forms.CharField(label='login',max_length=50)
-    senha = forms.CharField(label='senha',max_length=50,widget=forms.PasswordInput())
-    
-    def clean_login(self):
-        login = self.cleaned_data['login']
-        return login
-        
+class LoginForm(forms.Form):
+    login = forms.CharField(label='Usuário', max_length=50)
+    senha = forms.CharField(label='Senha', max_length=50, widget=forms.PasswordInput())
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('login')
+        password = cleaned_data.get('senha')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise ValidationError("Usuário ou senha inválidos.")
+            cleaned_data['user'] = user  # guarda o usuário autenticado
+        return cleaned_data
